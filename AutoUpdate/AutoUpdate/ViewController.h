7775451cbf0ebe4169a8d0294dea3a7282babfa7
Sir-Mr-Bman <sirmrbman@gmail.com>
2013-12-05 14:59:37 -0500

    Initial commit of the Xcode project

---
+++ AutoUpdate/AutoUpdate/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AutoUpdate
+//
+//  Created by Brennan Bman on 12/5/13.
+//  Copyright (c) 2013 Brennan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AutoUpdate/AutoUpdate/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AutoUpdate
+//
+//  Created by Brennan Bman on 12/5/13.
+//  Copyright (c) 2013 Brennan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
